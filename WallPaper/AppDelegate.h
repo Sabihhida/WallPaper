13f3ae3953612c96db1b635dd987df03ebfa0354
Sabih Hida Muhammad Tahir <sabih@cygnismedia.com>
2016-12-21 13:59:58 +0500

    Initial Commit

---
+++ WallPaper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WallPaper
+//
+//  Created by Cygnis Media on 21/12/2016.
+//  Copyright © 2016 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
